03a99101fab72c51db5f6f927d7c8a7069dda44f
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-03 23:10:52 +0300

    Update customer model

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
+CUSTOMER_EMAIL_MAX_LEN: int = 50
+
 
 class Customer(models.Model):
-    email = models.CharField(max_length=255,blank=False, null=False)
+    """
+    Класс для представления покупателей.
+
+    Метод __str__ возвращает электронную почту покупателя:
+        "some@email.com"
+
+    Сортировка производится по убыванию ID.
+
+    Атрибуты:
+        - email: str
+            - электронная почта покупателя
+            - валидируется на соответствие формата электронной почты
+    """
+    email = models.EmailField(
+        max_length=CUSTOMER_EMAIL_MAX_LEN,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
+        ordering = ('id',)
+
+    def str(self):
+        return self.email
