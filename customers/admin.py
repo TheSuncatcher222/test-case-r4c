f93fbc015bc122e4cc71ed4e87ebbd502acd2490
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-03 22:10:05 +0300

    Register Customer

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели Robot.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID (pk)
+            - электронная почта (email)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - электронная почта (email)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'email',
+    )
+    search_fields = (
+        'email',
+    )
+    list_per_page = 15
+
+
+admin.site.register(Customer, CustomerAdmin)
