6a9543fc9a5e52f4a080fcd6d647a39517d079c3
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-04 13:01:27 +0300

    Include customers and robots urls

--- R4C/urls.py
+++ R4C/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from customers.urls import urlpatterns as customers_urlpatterns
+from robots.urls import urlpatterns as robots_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(customers_urlpatterns)),
+    path('', include(robots_urlpatterns)),
 ]
