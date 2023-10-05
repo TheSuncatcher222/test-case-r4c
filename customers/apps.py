06ca14f3d3d6638aa077679c83cbe79c999c1664
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-05 18:09:49 +0300

    Register robot_post_save_handler

--- customers/apps.py
+++ customers/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class CustomersConfig(AppConfig):
     name = 'customers'
+
+    def ready(self):
+        from .signals import robot_post_save_handler
+
+        post_save.connect(
+            receiver=robot_post_save_handler,
+            dispatch_uid="path.to.this.module"
+        )
