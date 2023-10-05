1b7418e9a787ad25dc6741a6e11460feba8b5e15
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-05 11:13:38 +0300

    Add robots_urlpatterns

--- R4C/urls.py
+++ R4C/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from robots.urls import urlpatterns as robots_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(robots_urlpatterns))
 ]
