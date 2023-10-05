07f921713334ab03fc345fef01b7a863356bde89
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-05 11:12:29 +0300

    Add user settings

---
+++ R4C/core.py
@@ -0,0 +1,13 @@
+"""Excel export settings."""
+
+
+SEVEN_DAYS: int = 7
+EXCEL_FILEPATH: str = 'robots/'
+EXCEL_FILENAME: str = 'summary_for_week_robots.xlsx'
+EXCEL_FULL_PATH: str = f'{EXCEL_FILEPATH}{EXCEL_FILENAME}'
+
+"""HTTP response status codes."""
+
+
+STATUS_CODE_OK: int = 200
+STATUS_CODE_NOT_ALLOWED: int = 405
